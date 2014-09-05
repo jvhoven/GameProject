8b8c219851f12a542d32482bf90c2ca08532b2a7
Jeffrey van Hoven <wolferine@live.se>
2014-09-05 16:47:53 +0100

    Added gamestate/manager

---
+++ src/Gamestate/Manager.java
@@ -0,0 +1,15 @@
+package Gamestate;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Jeffrey
+ */
+public class Manager {
+    
+}
